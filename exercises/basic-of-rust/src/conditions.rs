12bfba7cf882c61ffb53c44f130b13fd2a8117bc
KietHuynh <hctuankiet243@gmail.com>
2023-07-05 23:32:55 +0700

    upload exercise

--- exercises/basic-of-rust/src/conditions.rs
+++ exercises/basic-of-rust/src/conditions.rs
@@ -1,18 +1,33 @@
+// use std::intrinsics::sqrtf32;
+
 //Exercise 1
 // Complete this function to return the bigger number!
 // Do not use:
 // - another function call
 // - additional variables
 pub fn bigger(a: i32, b: i32) -> i32 {
-    todo!()
+    // todo!() 
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 //Exercise 2
 // Input: Provide an arbitrary value of number
 // Check number is Positive or Negative or Zero
 // Output: &str
-fn check_number(number: u32) -> &'static str {
-    todo!()
+fn check_number(number:i32) -> &'static str {
+    // todo!()
+    if number > 0
+    {
+        "Positive"
+    } else if number < 0 {
+        "Negative"
+    } else {
+        "Zero"
+    }
 }
 
 // Exercise 3
@@ -23,7 +38,12 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
     } else {
-        1
+        if fizzish == "fuzz"
+        {
+            "bar"
+        } else {
+            "baz"
+        }
     }
 }
 
@@ -31,14 +51,25 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
 // Determine if a given year is a leap year
 // Implement logic
 fn is_leap_year(year: i32) -> bool {
-    todo!()
+    if year % 4 != 0 {
+        return false;
+    } else if year % 100 != 0 {
+        return true;
+    } else if year % 400 != 0 {
+        return false;
+    }
+    true
 }
 
 // Exercise 5
 // Calculate the factorial of a number
 // Implement logic
 fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut result = 1;
+    for i in 1..=n {
+        result *= i;
+    }
+    result
 }
 
 // Exercise 6
@@ -46,7 +77,18 @@ fn factorial(n: u32) -> u32 {
 // Implement logic
 
 fn is_prime(n: u32) -> bool {
-    todo!()
+    if n <= 1 {
+        return false;
+    }
+
+    let sqrt_n = (n as f64).sqrt() as u32;
+    for i in 2..=sqrt_n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    true
 }
 
 
